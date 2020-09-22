59545914a954c6813d73a8bf47aae46d1752308c
Hubert Nastały <hubertnastaly@Huberts-MBP.localdomain>
2020-09-22 15:50:15 +0200

    Adjust IRewardableValidators to Solidity v5

--- contracts/interfaces/IRewardableValidators.sol
+++ contracts/interfaces/IRewardableValidators.sol
@@ -4,7 +4,7 @@ interface IRewardableValidators {
     function isValidator(address _validator) external view returns (bool);
     function requiredSignatures() external view returns (uint256);
     function owner() external view returns (address);
-    function validatorList() external view returns (address[]);
+    function validatorList() external view returns (address[] memory);
     function getValidatorRewardAddress(address _validator) external view returns (address);
     function validatorCount() external view returns (uint256);
     function getNextValidator(address _address) external view returns (address);
