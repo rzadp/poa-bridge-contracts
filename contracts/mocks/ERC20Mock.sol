725b56213bd1e2770339c50ebfc7a978296b40b0
Hubert Nastały <hubertnastaly@Huberts-MBP.localdomain>
2020-09-22 17:27:06 +0200

    Adjust ERC20Mock to Solidity v5

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.5.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract ERC20Mock is DetailedERC20, BurnableToken, MintableToken {
-    constructor(string _name, string _symbol, uint8 _decimals) public DetailedERC20(_name, _symbol, _decimals) {
+contract ERC20Mock is ERC20Detailed, ERC20Burnable, ERC20Mintable, Ownable {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) public ERC20Detailed(_name, _symbol, _decimals) {
         // solhint-disable-previous-line no-empty-blocks
     }
 
     modifier hasMintPermission() {
-        require(msg.sender == owner || msg.sender == 0x06AF07097C9Eeb7fD685c692751D5C66dB49c215);
+        require(msg.sender == owner() || msg.sender == 0x06AF07097C9Eeb7fD685c692751D5C66dB49c215);
         _;
     }
 }
