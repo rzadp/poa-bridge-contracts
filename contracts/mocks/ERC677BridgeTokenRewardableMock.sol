87033c83d459418742f2486d2ba98ca986268e68
Hubert Nastały <hubertnastaly@Huberts-MBP.localdomain>
2020-09-22 17:28:21 +0200

    Adjust ERC677BridgeTokenRewardableMock to Solidity v5

--- contracts/mocks/ERC677BridgeTokenRewardableMock.sol
+++ contracts/mocks/ERC677BridgeTokenRewardableMock.sol
@@ -5,7 +5,7 @@ import "../ERC677BridgeTokenRewardable.sol";
 contract ERC677BridgeTokenRewardableMock is ERC677BridgeTokenRewardable {
     uint256 private _blockTimestamp;
 
-    constructor(string _name, string _symbol, uint8 _decimals, uint256 _chainId)
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _chainId)
         public
         ERC677BridgeTokenRewardable(_name, _symbol, _decimals, _chainId)
     {
