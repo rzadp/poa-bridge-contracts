ba055e2ac2a382232d19871be6ea65c903011158
Hubert Nastały <hubertnastaly@Huberts-MBP.localdomain>
2020-09-23 09:32:39 +0200

    Adjust ForeignBridgeErcToNative to Solidity v5

--- contracts/upgradeable_contracts/erc20_to_native/ForeignBridgeErcToNative.sol
+++ contracts/upgradeable_contracts/erc20_to_native/ForeignBridgeErcToNative.sol
@@ -11,14 +11,14 @@ contract ForeignBridgeErcToNative is BasicForeignBridge, ERC20Bridge, OtherSideB
         address _erc20token,
         uint256 _requiredBlockConfirmations,
         uint256 _gasPrice,
-        uint256[3] _dailyLimitMaxPerTxMinPerTxArray, // [ 0 = _dailyLimit, 1 = _maxPerTx, 2 = _minPerTx ]
-        uint256[2] _homeDailyLimitHomeMaxPerTxArray, //[ 0 = _homeDailyLimit, 1 = _homeMaxPerTx ]
+        uint256[3] calldata _dailyLimitMaxPerTxMinPerTxArray, // [ 0 = _dailyLimit, 1 = _maxPerTx, 2 = _minPerTx ]
+        uint256[2] calldata _homeDailyLimitHomeMaxPerTxArray, //[ 0 = _homeDailyLimit, 1 = _homeMaxPerTx ]
         address _owner,
         int256 _decimalShift,
         address _bridgeOnOtherSide
     ) external onlyRelevantSender returns (bool) {
         require(!isInitialized());
-        require(AddressUtils.isContract(_validatorContract));
+        require(Address.isContract(_validatorContract));
         require(_owner != address(0));
         require(_bridgeOnOtherSide != address(0));
 
