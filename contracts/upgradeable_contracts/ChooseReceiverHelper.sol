6998cc0905faf6b0c42f2e48c36143448ba60f99
Hubert Nastały <hubertnastaly@Huberts-MBP.localdomain>
2020-09-23 09:33:50 +0200

    Adjust ChooseReceiverHelper to Solidity v5

--- contracts/upgradeable_contracts/ChooseReceiverHelper.sol
+++ contracts/upgradeable_contracts/ChooseReceiverHelper.sol
@@ -9,7 +9,7 @@ contract ChooseReceiverHelper {
     * @param _data passed data in the transfer message.
     * @return address of the receiver on the other side.
     */
-    function chooseReceiver(address _from, bytes _data) internal view returns (address recipient) {
+    function chooseReceiver(address _from, bytes memory _data) internal view returns (address recipient) {
         recipient = _from;
         if (_data.length > 0) {
             require(_data.length == 20);
