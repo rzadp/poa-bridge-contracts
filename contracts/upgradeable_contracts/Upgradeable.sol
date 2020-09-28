0174f4f75cbf5688fb83b209db73fa3b6866b6cd
Hubert Nastały <hubertnastaly@Huberts-MBP.localdomain>
2020-09-28 15:21:46 +0200

    Cast this to address in Upgradeable

--- contracts/upgradeable_contracts/Upgradeable.sol
+++ contracts/upgradeable_contracts/Upgradeable.sol
@@ -5,7 +5,7 @@ import "../interfaces/IUpgradeabilityOwnerStorage.sol";
 contract Upgradeable {
     // Avoid using onlyUpgradeabilityOwner name to prevent issues with implementation from proxy contract
     modifier onlyIfUpgradeabilityOwner() {
-        require(msg.sender == IUpgradeabilityOwnerStorage(this).upgradeabilityOwner());
+        require(msg.sender == IUpgradeabilityOwnerStorage(address(this)).upgradeabilityOwner());
         /* solcov ignore next */
         _;
     }
