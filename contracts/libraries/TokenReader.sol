d9553ce28065da00132120c44b1b35fa547516dc
Hubert Nastały <hubertnastaly@Huberts-MBP.localdomain>
2020-09-28 12:17:34 +0200

    Adjust assembly commands in TokenReader to Solidity v5

--- contracts/libraries/TokenReader.sol
+++ contracts/libraries/TokenReader.sol
@@ -20,8 +20,7 @@ library TokenReader {
             mstore(ptr, 0x06fdde0300000000000000000000000000000000000000000000000000000000) // name()
             if iszero(staticcall(gas, _token, ptr, 4, ptr, 32)) {
                 mstore(ptr, 0xa3f4df7e00000000000000000000000000000000000000000000000000000000) // NAME()
-                staticcall(gas, _token, ptr, 4, ptr, 32)
-                pop
+                pop(staticcall(gas, _token, ptr, 4, ptr, 32))
             }
 
             mstore(0x40, add(ptr, returndatasize))
@@ -37,23 +36,26 @@ library TokenReader {
         }
         string memory res = new string(size);
         assembly {
-            if gt(returndatasize, 32) {
+
+            switch gt(returndatasize, 32)
+            case 1 {
                 // load as string
                 returndatacopy(add(res, 32), 64, size)
-                jump(exit)
             }
-            /* solhint-disable */
-            if gt(returndatasize, 0) {
-                let i := 0
-                ptr := mload(ptr) // load bytes32 value
-                mstore(add(res, 32), ptr) // save value in result string
+            case 0 {
+                /* solhint-disable */
+                if gt(returndatasize, 0) {
+                    let i := 0
+                    ptr := mload(ptr) // load bytes32 value
+                    mstore(add(res, 32), ptr) // save value in result string
 
-                for { } gt(ptr, 0) { i := add(i, 1) } { // until string is empty
-                    ptr := shl(8, ptr) // shift left by one symbol
+                    for { } gt(ptr, 0) { i := add(i, 1) } { // until string is empty
+                        ptr := shl(8, ptr) // shift left by one symbol
+                    }
+                    mstore(res, i) // save result string length
                 }
-                mstore(res, i) // save result string length
             }
-            exit:
+
             /* solhint-enable */
         }
         return res;
@@ -74,8 +76,7 @@ library TokenReader {
             mstore(ptr, 0x95d89b4100000000000000000000000000000000000000000000000000000000) // symbol()
             if iszero(staticcall(gas, _token, ptr, 4, ptr, 32)) {
                 mstore(ptr, 0xf76f8d7800000000000000000000000000000000000000000000000000000000) // SYMBOl()
-                staticcall(gas, _token, ptr, 4, ptr, 32)
-                pop
+                pop(staticcall(gas, _token, ptr, 4, ptr, 32))
             }
 
             mstore(0x40, add(ptr, returndatasize))
@@ -91,23 +92,25 @@ library TokenReader {
         }
         string memory res = new string(size);
         assembly {
-            if gt(returndatasize, 32) {
+            switch gt(returndatasize, 32)
+            case 1 {
                 // load as string
                 returndatacopy(add(res, 32), 64, size)
-                jump(exit)
             }
-            /* solhint-disable */
-            if gt(returndatasize, 0) {
-                let i := 0
-                ptr := mload(ptr) // load bytes32 value
-                mstore(add(res, 32), ptr) // save value in result string
+            case 0 {
+                /* solhint-disable */
+                if gt(returndatasize, 0) {
+                    let i := 0
+                    ptr := mload(ptr) // load bytes32 value
+                    mstore(add(res, 32), ptr) // save value in result string
 
-                for { } gt(ptr, 0) { i := add(i, 1) } { // until string is empty
-                    ptr := shl(8, ptr) // shift left by one symbol
+                    for { } gt(ptr, 0) { i := add(i, 1) } { // until string is empty
+                        ptr := shl(8, ptr) // shift left by one symbol
+                    }
+                    mstore(res, i) // save result string length
                 }
-                mstore(res, i) // save result string length
             }
-            exit:
+
             /* solhint-enable */
         }
         return res;
