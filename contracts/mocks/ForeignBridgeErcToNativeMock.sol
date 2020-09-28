7ce5fe935b1f28efe407bb70043b5f82f67964c5
Hubert Nastały <hubertnastaly@Huberts-MBP.localdomain>
2020-09-28 15:20:04 +0200

    Cast tokens to addresses in ForeignBridgeErcToNativeMock

--- contracts/mocks/ForeignBridgeErcToNativeMock.sol
+++ contracts/mocks/ForeignBridgeErcToNativeMock.sol
@@ -6,7 +6,7 @@ contract ForeignBridgeErcToNativeMock is ForeignBridgeErcToNative {
     bytes32 internal constant CHAI_TOKEN_MOCK = 0x5d6f4e61a116947624416975e8d26d4aff8f32e21ea6308dfa35cee98ed41fd8; // keccak256(abi.encodePacked("chaiTokenMock"))
 
     function setChaiToken(IChai _chaiToken) external {
-        addressStorage[CHAI_TOKEN_MOCK] = _chaiToken;
+        addressStorage[CHAI_TOKEN_MOCK] = address(_chaiToken);
     }
 
     function chaiToken() public view returns (IChai) {
