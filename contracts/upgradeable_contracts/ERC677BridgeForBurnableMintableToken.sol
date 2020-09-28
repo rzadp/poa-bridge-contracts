edda04df171c828122f1c873445c11b1d0578462
Hubert Nastały <hubertnastaly@Huberts-MBP.localdomain>
2020-09-28 15:26:33 +0200

    Cast token to address in ERC677BridgeForBurnableMintableToken

--- contracts/upgradeable_contracts/ERC677BridgeForBurnableMintableToken.sol
+++ contracts/upgradeable_contracts/ERC677BridgeForBurnableMintableToken.sol
@@ -5,7 +5,7 @@ import "../interfaces/IBurnableMintableERC677Token.sol";
 
 contract ERC677BridgeForBurnableMintableToken is ERC677Bridge {
     function bridgeSpecificActionsOnTokenTransfer(ERC677 _token, address _from, uint256 _value, bytes memory _data) internal {
-        IBurnableMintableERC677Token(_token).burn(_value);
+        IBurnableMintableERC677Token(address(_token)).burn(_value);
         fireEventOnTokenTransfer(chooseReceiver(_from, _data), _value);
     }
 }
